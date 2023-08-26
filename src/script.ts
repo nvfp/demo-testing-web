baf82b6c44efeddb3a17ecb31d3f671b9574d330
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-26 11:53:05 -0700

    save

--- src/script.ts
+++ src/script.ts
@@ -1,19 +1,26 @@
-import rand
+// import rand
 
 
-console.log(randInt(3, 10))
+// console.log(randInt(3, 10))
 
 
-// Get the canvas element
-const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+// // Get the canvas element
+// const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
 
-// Check if the canvas element exists
-if (canvas) {
-    const context = canvas.getContext('2d');
+// // Check if the canvas element exists
+// if (canvas) {
+//     const context = canvas.getContext('2d');
 
-    if (context) {
-        // Your canvas drawing code goes here
-        context.fillStyle = 'blue';
-        context.fillRect(10, 10, 50, 50);
-    }
-}
+//     if (context) {
+//         // Your canvas drawing code goes here
+//         context.fillStyle = 'blue';
+//         context.fillRect(10, 10, 50, 50);
+//     }
+// }
+
+
+
+import { randInt } from "wondering/random"
+import { test} from "wondering"
+
+console.log(randInt(2, 5))
